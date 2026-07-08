4f3a4f2b84a8c85be000027006e0e722b42dd3a3
jackie jan <zakaria.farah55@outlook.com>
2026-07-08 11:12:28 +0300

    refactor: remove comments and export asignment through the editor

--- server/queue_manager.gd
+++ server/queue_manager.gd
@@ -1,8 +1,7 @@
 extends Node
 class_name QueueManager
 var waiting_players: Array[int] = []
-@onready var Match = preload("res://server/Match.tscn")
-@onready var matchManager = $"../MatchManager"
+@export var matchManager: MatchManager
 
 func _ready() -> void:
 	Network.join_queue_requested.connect(enqueue_player)
@@ -15,10 +14,8 @@ func enqueue_player(peer_id: int) -> void:
 	if waiting_players.size() >= 2:
 		var peerId = waiting_players.pop_front()
 		var peerId2 = waiting_players.pop_front()
-		var matchState = MatchState.new()
-		matchState.player_ids.append_array([peerId, peerId2])
 		matchManager.create_new_match(peerId, peerId2)
-		#Network.create_match_requested.emit(matchState)
 
 func remove_player(peer_id: int) -> void:
 	waiting_players.erase(peer_id)
+	
