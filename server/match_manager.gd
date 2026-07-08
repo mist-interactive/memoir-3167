b49471f4f7613e089f8735ac0570a9a2d998ac2c
jackie jan <zakaria.farah55@outlook.com>
2026-07-08 11:11:27 +0300

    refactor(matchManager): improved the abstraction and client connection state

--- server/match_manager.gd
+++ server/match_manager.gd
@@ -1,34 +1,41 @@
 extends Node
+class_name MatchManager
 var _next_match_id: int = 0
-var peer_to_match: Dictionary = {}
-var matches: Dictionary = {}
+var peer_to_match: Dictionary[int, int] = {}
+var matches: Dictionary[int, matchController] = {}
 
 func _ready() -> void:
-	pass
+	Network.Match.connect_match_requested.connect(_on_player_connect)
 
 func create_new_match(peerId1: int, peerId2: int) -> void:
 	print("Call to create new match")
-	var matchState = MatchState.new()
-	matchState.matchId = _next_match_id
-	matchState.player_ids.append_array([peerId1, peerId2])
+	var matchState = MatchState.new(_next_match_id, [peerId1, peerId2])
+	var battleField = Battlefield.new("map.json")
 	
-	var gameState = GameState.new()
-	gameState.player_turn = peerId1
-	
-	var matchNode = Node.new()
+	var matchNode = matchController.new(matchState, battleField)
 	matchNode.name = "Match_%d" % _next_match_id
-	matchNode.add_child(gameState)
 	matchNode.add_child(matchState)
+	matchNode.add_child(battleField)
 	get_parent().add_child(matchNode)
-	peer_to_match[peerId1] = matchNode
-	peer_to_match[peerId2] = matchNode
+	peer_to_match[peerId1] = matchState.matchId
+	peer_to_match[peerId2] = matchState.matchId
 	matches[_next_match_id] = matchNode
 	++_next_match_id
-	Network.create_match.rpc_id(peerId1, matchState.matchId, peerId1)
-	Network.create_match.rpc_id(peerId2, matchState.matchId, peerId1)
-	
-func get_peer_match_state(peer: int) -> MatchState:
-	return peer_to_match[peer].get_node("matchState")
+	Network.Match.match_created.rpc_id(peerId1)
+	Network.Match.match_created.rpc_id(peerId2)
 
-func get_peer_game_state(peer: int) -> GameState:
-	return peer_to_match[peer].get_node("gameState")
+# signals handlers
+func _on_player_connect(peer_id: int) -> void:
+	var matchId: int = peer_to_match[peer_id]
+	var matchCtl: matchController = matches[matchId]
+	matchCtl.handle_connect(peer_id)
+	
+func _on_player_disconnect(peed_id: int) -> void:
+	var matchId: int = peer_to_match[peed_id]
+	var matchCtl: matchController = matches[matchId]
+	matchCtl.handle_disconnect(peed_id)
+
+func _on_connect_match_requested(peer_id: int) -> void:
+	var matchId: int = peer_to_match[peer_id]
+	var matchCtl: matchController = matches[matchId]
+	Network.Match.init(matchId, matchCtl.battlefield.mapName, matchCtl.matchState.player_ids)
