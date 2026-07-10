9b3786bbca12a3efe6d2cdf638326979a2f33edb
Usva Pellinen <julius.pellinen@gmail.com>
2026-07-10 15:33:24 +0300

    Fix:
    moved deck_manager logic from match_manager to its own file

--- server/match_manager.gd
+++ server/match_manager.gd
@@ -13,13 +13,13 @@ func create_new_match(peerId1: int, peerId2: int) -> void:
 	print("Call to create new match")
 	var matchState = MatchState.new(_next_match_id, [peerId1, peerId2])
 	var battleField = BattlefieldState.new("map.json")
+	var deckManager = DeckManager.new(peerId1, peerId2)
 	
 	var matchNode = matchController.new(matchState, battleField)
 	matchNode.name = "Match_%d" % _next_match_id
 	matchNode.add_child(matchState)
 	matchNode.add_child(battleField)
-	# creates deck and adds child
-	create_deck(peerId1, peerId2, matchNode)
+	matchNode.add_child(deckManager)
 	get_parent().add_child(matchNode)
 	peer_to_match[peerId1] = matchState.matchId
 	peer_to_match[peerId2] = matchState.matchId
@@ -43,11 +43,3 @@ func _on_connect_match_requested(peer_id: int) -> void:
 	var matchId: int = peer_to_match[peer_id]
 	var matchCtl: matchController = matches[matchId]
 	Network.Match.init(matchId, matchCtl.battlefield.mapName, matchCtl.matchState.player_ids)
-
-# creates an array of card ids
-func create_deck(player1: int, player2: int, matchNode: matchController) -> void:
-	var raw_ids: Array = card_database.card_registry.keys()
-	var starting_cards: Array[String] = []
-	starting_cards.assign(raw_ids)
-	var deck_manager = DeckManager.new(player1, player2, starting_cards)
-	matchNode.add_child(deck_manager)
