18987c30335525a211be6d0c426e6a20e1c75936
jackie jan <zakaria.farah55@outlook.com>
2026-07-13 17:26:12 +0300

    feat(game): Added loading screen

--- client/game.gd
+++ client/game.gd
@@ -1,12 +1,18 @@
 extends Node
 @onready var renderGameBoard = preload("res://client/battlefield/battlefield.tscn")
+const LoaderScene = preload("res://client/ui/loading.tscn")
+@export var loader: Loader
+var battlefieldRenderer: Node2D
+@export var menu: Node
 
 func _ready() -> void:
-	print("here")
 	Network.Match.init_match_requested.connect(create_new_match)
 
-func create_new_match(battleField: BattlefieldState, matchState: MatchState):
-	print("newMatch: ", matchState.matchId)
-	add_child(battleField)
-	add_child(matchState)
-	add_child(renderGameBoard.instantiate())
+func create_new_match(matchId: int, mapName: String, player_ids: Array[int]):
+	menu.queue_free()
+	await loader.stage("Initializing battlefield...", func(): add_child(BattlefieldState.new(mapName))) \
+	.stage("Initializing matchstate...", func(): add_child(MatchState.new(matchId, player_ids))) \
+	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); Network.Match.update_client_match_state.rpc(MatchState.STATE.READY)) \
+	.stage("Waiting oponents to ready up...", func(): await loader.wait_untill(func(): return get_child(1).state == MatchState.STATE.READY)) \
+	.run()
+	add_child(battlefieldRenderer)
