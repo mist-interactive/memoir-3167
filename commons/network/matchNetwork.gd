66f36e1a52faf81463c263d547e94b67c15f8048
jackie jan <zakaria.farah55@outlook.com>
2026-07-14 13:13:46 +0300

    feat(game): syncing matchsate rpc

--- commons/network/matchNetwork.gd
+++ commons/network/matchNetwork.gd
@@ -23,16 +23,18 @@ func init(matchId: int, mapName: String, player_ids: Array[int]) -> void:
 		return
 	init_match_requested.emit(matchId, mapName, player_ids)
 
+signal sync_requested(peer_id: int, snapshot: Dictionary)
+@rpc("authority", "call_remote")
+func sync(snapshot: Dictionary) -> void:
+	if multiplayer.is_server():
+		return
+	print("server wants to sync")
+	sync_requested.emit(snapshot)
+
+# client actions
 signal update_client_match_change_requested(peer_id: int, state: MatchState.STATE)
 @rpc("any_peer", "call_remote")
 func update_client_match_state(state: MatchState.STATE):
 	if !multiplayer.is_server():
 		return
 	update_client_match_change_requested.emit(multiplayer.get_remote_sender_id(), state)
-
-signal match_state_change_requested(state: MatchState.STATE)
-@rpc("authority", "call_remote")
-func update_match_state(state: MatchState.STATE) -> void:
-	if multiplayer.is_server():
-		return
-	match_state_change_requested.emit(state)
