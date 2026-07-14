bd89781156444d467004495f54960b77889c6953
jackie jan <zakaria.farah55@outlook.com>
2026-07-14 13:13:27 +0300

    feat(game): syncing matchsate, initial turn phases

--- commons/state/MatchState.gd
+++ commons/state/MatchState.gd
@@ -2,20 +2,60 @@ extends Node
 class_name MatchState
 var matchId: int
 var player_ids: Array[int]
-var scores: Dictionary[int, int]	
-var state: STATE = STATE.INITIALIZING
+var scores: Dictionary[int, int]
+var state: STATE = STATE.INITIALIZING:
+	set(newState):
+		state = newState
+		should_sync = true
+var phase: TURN_PHASE = TURN_PHASE.START:
+	set(newPhase):
+		phase = newPhase
+		should_sync = true
+var player_turn_index: int:
+	set(newIndex):
+		player_turn_index = newIndex
+		should_sync = true
+var should_sync: bool = true
 enum STATE {INITIALIZING, READY, IN_PROGRESS, PAUSED, ENDED}
+enum TURN_PHASE {START, PLAY_CARD, ISSUE_ORDERS, EXECUTE_ORDERS, DRAW_CARD}
 
-func _init(matchId: int, player_ids: Array[int]) -> void:
+func _init() -> void:
 	name = "matchState"
+	Network.Match.sync_requested.connect(_on_sync)
+
+func initialize(matchId: int, player_ids: Array[int]) -> void:
 	self.matchId = matchId
 	self.player_ids = player_ids
+	self.player_turn_index = randi_range(0,1)
 	self.scores[player_ids[0]] = 0
-	self.scores[player_ids[0]] = 0
-	Network.Match.match_state_change_requested.connect(_on_state_change)
-	
+	self.scores[player_ids[1]] = 0
+	self.phase = TURN_PHASE.START
 
-# signal handlers
-func _on_state_change(state: MatchState.STATE):
-	print("Upding client state")
-	self.state = state
+func get_snapshot() -> Dictionary:
+	return {
+		"matchId": self.matchId,
+		"player_ids": self.player_ids,
+		"scores": self.scores,
+		"state": self.state,
+		"phase": self.phase,
+		"player_turn_index": self.player_turn_index
+	}
+
+func _on_sync(snapshot: Dictionary):
+	print("syncing with server")
+	matchId = snapshot.matchId
+	player_ids = snapshot.player_ids
+	scores = snapshot.scores
+	state = snapshot.state
+	phase = snapshot.phase
+	player_turn_index = snapshot.player_turn_index
+
+func sync() -> void:
+	if should_sync:
+		Network.Match.sync.rpc_id(player_ids[0], get_snapshot())
+		Network.Match.sync.rpc_id(player_ids[1], get_snapshot())
+		should_sync = false
+
+func is_player_turn(player_id: int) -> bool:
+	var index: int = player_ids.find(player_id)
+	return index == player_turn_index
