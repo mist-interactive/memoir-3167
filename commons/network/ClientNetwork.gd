b54985c9afe0e7768fd22c4feeb0b973e13004f6
jackie jan <zakaria.farah55@outlook.com>
2026-07-14 13:14:09 +0300

    feat(game): added client abstraction class rpc calls

---
+++ commons/network/ClientNetwork.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name ClientNetwork
+
+signal auth_check_requested(peer_id: int, jtw_token: String)
+@rpc("any_peer", "call_remote")
+func auth_check(jtw_token: String) -> void:
+	if !multiplayer.is_server():
+		return
+	auth_check_requested.emit(multiplayer.get_remote_sender_id(), jtw_token)
+
+signal sync_requested(snapshot: Dictionary)
+@rpc("authority", "call_remote")
+func sync(snapshot: Dictionary) -> void:
+	if multiplayer.is_server():
+		return
+	print("Client sync requested")
+	sync_requested.emit(snapshot)
