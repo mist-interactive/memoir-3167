81b789588a1f82256165063b363e357684ec20e1
Usva Pellinen <julius.pellinen@gmail.com>
2026-07-14 22:19:06 +0300

    fix:
    added default target to target

--- commons/cards/command_card/CommandCard.gd
+++ commons/cards/command_card/CommandCard.gd
@@ -5,7 +5,7 @@ extends Resource
 @export var id: String = ""
 @export var display_name: String = ""
 @export_multiline var description: String = ""
-@export var card_target: GameEnums.Sector
+@export var card_target: GameEnums.Sector = GameEnums.Sector.ALL
 @export var quantity: int = 1
 # any other variables the cards might need
 #
