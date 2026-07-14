d565cc132fa0308a9985454225a8a96abd01a263
jackie jan <zakaria.farah55@outlook.com>
2026-07-14 13:20:16 +0300

    feat(server): server keeps track of client and dummy auth checks

--- server/server.gd
+++ server/server.gd
@@ -1,24 +1,35 @@
 extends Node
 var peer: WebSocketMultiplayerPeer
 var port: int = 6669
-var peers: Array[int]
+var clients: Dictionary[int, Client]
+var sessions: Dictionary[int, int] # uuid -> peer_id
 
 func _ready() -> void:
 	name = "SERVER"
 
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	Network.Client.auth_check_requested.connect(_on_auth_check)
 
 	peer = WebSocketMultiplayerPeer.new()
 	peer.create_server(port)
 	multiplayer.multiplayer_peer = peer
 	print("Server started")
-	
+
+func _physics_process(delta: float) -> void:
+	for peer_id in clients.keys():
+		var client: Client = clients[peer_id]
+		client.sync()
 
 func _on_peer_connected(id: int) -> void:
 	print("A new client has connected id: ", id)
-	peers.append(id)
+	clients[id] = Client.new(id)
 
 func _on_peer_disconnected(id: int) -> void:
 	print("Client has disconnected: ", id)
-	peers.erase(id)
+	clients.erase(id)
+
+func _on_auth_check(peer_id: int, jtw_token: String) -> void:
+	print("Authenticating client %d..." % peer_id)
+	var client: Client = clients[peer_id]
+	client.authenticated = true
