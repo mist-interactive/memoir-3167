d8b8d0ba295b73f3117f557148510afb55561b5d
Antti Pollanen <antti.pollanen89@gmail.com>
2026-07-16 16:01:40 +0300

    wip(UnitManager): UnitManager has both coord based and unit_uuid based Dictionaries

--- commons/units/unit_manager.gd
+++ commons/units/unit_manager.gd
@@ -1,15 +1,20 @@
+# UnitManager is autoloaded and is globally accessible
 extends Node
 
 # The Unit Grid:
 # Key = Vector2i (hex coord), Value = Unit
 var unit_grid: Dictionary = {}
+var units_by_id: Dictionary = {}
 
 func add_unit(unit: Node2D, coord: Vector2i) -> void:
 	unit_grid[coord] = unit
+	units_by_id[unit.unit_uuid] = unit
 	print("Unit registered at ", coord, " | Total units: ", unit_grid.size())
 	
 func remove_unit(coord: Vector2i) -> void:
 	if unit_grid.has(coord):
+		var unit_to_remove = unit_grid[coord]
+		units_by_id.erase(unit_to_remove.unit_uuid)
 		unit_grid.erase(coord)
 
 func move_unit(unit: Node2D, old_coord: Vector2i, new_coord: Vector2i) -> void:
@@ -18,3 +23,6 @@ func move_unit(unit: Node2D, old_coord: Vector2i, new_coord: Vector2i) -> void:
 
 func get_unit_at(coord: Vector2i) -> Node2D:
 	return unit_grid.get(coord)
+	
+func get_unit_by_id(id: String) -> Node2D:
+	return units_by_id.get(id)
