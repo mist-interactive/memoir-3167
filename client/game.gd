6ced93ec797098698213a6bdaa03225236d495b0
Zakaria Farah <zfarah@c1r6p12.hive.fi>
2026-07-21 19:36:13 +0300

    refactor(game): typos, renames and changes suggested in the PR

--- client/game.gd
+++ client/game.gd
@@ -1,6 +1,5 @@
 extends Node
 @onready var renderGameBoard = preload("res://client/battlefield/battlefield.tscn")
-const LoaderScene = preload("res://client/ui/loading.tscn")
 @export var loader: Loader
 var battlefieldRenderer: Node2D
 @export var menu: Node2D
@@ -12,7 +11,7 @@ func create_new_match(matchId: int, mapName: String, player_ids: Array[int]):
 	menu.hide()
 	await loader.stage("Initializing battlefield...", func(): add_child(BattlefieldState.new(mapName))) \
 	.stage("Initializing matchstate...", func(): add_child(MatchState.new())) \
-	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); Network.Match.update_client_match_state.rpc(MatchState.STATE.READY)) \
+	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); Network.Match.update_client_match_state.rpc_id(1, MatchState.STATE.READY)) \
 	.stage("Waiting oponents to ready up...", func(): await loader.wait_untill(func(): return get_child(1).state == MatchState.STATE.READY)) \
 	.run()
 	add_child(battlefieldRenderer)
