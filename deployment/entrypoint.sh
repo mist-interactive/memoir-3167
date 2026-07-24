5a34c7c87a063ce25e8e455a658eb97ad4b75aca
Antti Pollanen <antti.pollanen89@gmail.com>
2026-07-24 14:51:57 +0300

    feat(Dockerfile): Build server in container using multistage build and run server

--- deployment/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-mkdir -p /root/build/server/
-godot --headless --path /root/memoir-3167 --export-release "Linux" "/root/build/server/memoir-3167.bin"
-/root/build/server/memoir-3167.bin
-
-exec "$@"
