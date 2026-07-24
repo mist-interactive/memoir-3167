d964c380af64ff9844957a120292de012476397f
Antti Pollanen <antti.pollanen89@gmail.com>
2026-07-24 14:27:03 +0300

    wip(unit manager): working on syncincg user clicks per match

--- commons/network/matchNetwork.gd
+++ commons/network/matchNetwork.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name MatchNetwork
 
+signal server_hex_requested(peer_id: int, hex: Vector2i)
+
 signal connect_match_requested
 @rpc("any_peer","call_remote")
 func connect_match() -> void:
@@ -38,3 +40,11 @@ func update_client_match_state(state: MatchState.STATE):
 	if !multiplayer.is_server():
 		return
 	update_client_match_change_requested.emit(multiplayer.get_remote_sender_id(), state)
+
+@rpc("any_peer", "call_remote", "reliable")
+func request_hex_selection(hex: Vector2i) -> void:
+	if !multiplayer.is_server():
+		return
+	print("hex selection requested")
+	var sender := multiplayer.get_remote_sender_id()
+	server_hex_requested.emit(sender, hex)
