7e54fce35385b1c9fb02bcef89d9b40706cd7bf1
Zakaria Farah <zakaria.farah55@outlook.com>
2026-07-27 14:53:18 +0300

    feat: added hand abstraction and syncing with clients

--- client/game.gd
+++ client/game.gd
@@ -11,7 +11,8 @@ func create_new_match(matchId: int, mapName: String, player_ids: Array[int]):
 	menu.hide()
 	await loader.stage("Initializing battlefield...", func(): add_child(BattlefieldState.new(mapName))) \
 	.stage("Initializing matchstate...", func(): add_child(MatchState.new())) \
+	.stage("Initializing handstate...", func(): add_child(HandState.new())) \
 	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); Network.Match.update_client_match_state.rpc_id(1, MatchState.STATE.READY)) \
-	.stage("Waiting oponents to ready up...", func(): await loader.wait_untill(func(): return get_child(1).state == MatchState.STATE.READY)) \
+	.stage("Waiting oponents to ready up...", func(): await loader.wait_untill(func(): return get_child(1).state == MatchState.STATE.IN_PROGRESS)) \
 	.run()
 	add_child(battlefieldRenderer)
