2c8496d2dcf03a593d63b0936be614ef0642a244
Zakaria Farah <zakaria.farah55@outlook.com>
2026-07-28 16:28:20 +0300

    fix: ui bug, seeing the game board while in loading screen

--- client/game.gd
+++ client/game.gd
@@ -12,9 +12,9 @@ func create_new_match(matchId: int, mapName: String, player_ids: Array[int]):
 	await loader.stage("Initializing battlefield...", func(): add_child(BattlefieldState.new(mapName))) \
 	.stage("Initializing matchstate...", func(): add_child(MatchState.new())) \
 	.stage("Initializing handstate...", func(): add_child(HandState.new())) \
-	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); add_child(battlefieldRenderer); Network.Match.update_client_match_state.rpc_id(1, MatchState.STATE.READY)) \
+	.stage("Loading map...", func(): battlefieldRenderer = renderGameBoard.instantiate(); Network.Match.update_client_match_state.rpc_id(1, MatchState.STATE.READY)) \
 	.stage("Waiting oponents to ready up...", func(): await loader.wait_untill(func(): return get_child(1).state == MatchState.STATE.IN_PROGRESS)) \
 	.run()
-	#add_child(battlefieldRenderer)
-	battlefieldRenderer.setup_hand_ui(player_ids)
+	add_child(battlefieldRenderer)
+	battlefieldRenderer.setup_hand_ui()
 	
