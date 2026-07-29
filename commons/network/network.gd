3d4c27ba55fb6d810f04bdb6a4e310f1507aa5f8
Antti Pollanen <antti.pollanen89@gmail.com>
2026-07-29 13:35:31 +0300

    wip(units): Commented out old hex selection logic

--- commons/network/network.gd
+++ commons/network/network.gd
@@ -16,6 +16,7 @@ func join_queue() -> void:
 signal hex_selected(peer_id: int, hex: Vector2i)
 signal hex_broadcast(peer_id: int, hex: Vector2i)
 
+"""
 @rpc("any_peer", "call_remote", "reliable")
 func request_hex_selection(hex: Vector2i) -> void:
 	# runs on server
@@ -32,3 +33,4 @@ func request_hex_selection(hex: Vector2i) -> void:
 func sync_hex_selection(peer_id: int, hex: Vector2i) -> void:
 	# runs on all peers
 	hex_broadcast.emit(peer_id, hex)
+"""
