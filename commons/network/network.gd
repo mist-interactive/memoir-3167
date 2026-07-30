5b96ae37631afcbd084e8690a66ec5cc7d7fcee0
Antti Pollanen <antti.pollanen89@gmail.com>
2026-07-30 11:09:39 +0300

    wip(removal): Removed unused code

--- commons/network/network.gd
+++ commons/network/network.gd
@@ -6,31 +6,8 @@ extends Node
 @export var Card: CardNetwork
 
 signal join_queue_requested(peer_id: int)
-signal server_hex_requested(peer_id:int, hex: Vector2i)
 
 @rpc("any_peer","call_remote")
 func join_queue() -> void:
 	print("calling to server")
 	join_queue_requested.emit(multiplayer.get_remote_sender_id())
-
-signal hex_selected(peer_id: int, hex: Vector2i)
-signal hex_broadcast(peer_id: int, hex: Vector2i)
-
-"""
-@rpc("any_peer", "call_remote", "reliable")
-func request_hex_selection(hex: Vector2i) -> void:
-	# runs on server
-	if !multiplayer.is_server():
-		return
-	var sender := multiplayer.get_remote_sender_id()
-	server_hex_requested.emit(sender, hex)
-#	hex_selected.emit(sender, hex)
-
-	# fan out to everyone
-#	sync_hex_selection.rpc(sender, hex)
-
-@rpc("authority", "call_local", "reliable")
-func sync_hex_selection(peer_id: int, hex: Vector2i) -> void:
-	# runs on all peers
-	hex_broadcast.emit(peer_id, hex)
-"""
