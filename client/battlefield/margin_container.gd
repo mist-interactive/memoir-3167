b321c5f3378fb5ef8adb7d5d7d9f1451d2f3d9c2
usva <julius.pellinen@gmail.com>
2026-08-04 11:47:54 +0300

    fix: updated hand margin container values

--- client/battlefield/margin_container.gd
+++ client/battlefield/margin_container.gd
@@ -3,4 +3,4 @@ extends MarginContainer
 func _ready() -> void:
 	var canvas_size: Vector2 = get_viewport_rect().size
 	custom_maximum_size = Vector2(canvas_size.x / 1.5, canvas_size.y / 1.5)
-	position = Vector2(canvas_size.x / 2, canvas_size.y)
+	position.y = canvas_size.y
--- client/battlefield/margin_container_2.gd
+++ client/battlefield/margin_container_2.gd
@@ -1,8 +1,5 @@
 extends MarginContainer
 
-var CARDSIZE = Vector2(140, 220)
-
 func _ready() -> void:
 	var canvas_size: Vector2 = get_viewport_rect().size
 	custom_maximum_size = Vector2(canvas_size.x / 1.5, canvas_size.y / 1.5)
-	position = Vector2(canvas_size.x / 2, CARDSIZE.y / 2)
