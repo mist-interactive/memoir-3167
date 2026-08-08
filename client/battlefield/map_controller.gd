b198499e5f89976b8d35567888d8e43fa90510f9
Mursupaani <antti.pollanen89@gmail.com>
2026-08-08 11:43:01 +0300

    feat(map): Select only hexes that are on the gameboard

--- client/battlefield/map_controller.gd
+++ client/battlefield/map_controller.gd
@@ -3,6 +3,8 @@ extends HexagonTileMapLayer
 @export var highlight_layer: TileMapLayer
 var player_hex := {}
 
+var map_limits
+
 func _ready() -> void:
 	assert(highlight_layer)
 	assert(Network)
@@ -12,7 +14,10 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var click_position: Vector2 = get_global_mouse_position()
 		var hex: Vector2i = local_to_map(to_local(click_position))
-		Network.Match.request_hex_selection.rpc_id(1, hex)
+		# Check if selected hex is actually on the gameboard
+		var cell_source_id := get_cell_source_id(hex)
+		if cell_source_id != -1:
+			Network.Match.request_hex_selection.rpc_id(1, hex)
 
 func _on_hex_broadcast(peer_id: int, hex: Vector2i) -> void:
 	if peer_id == multiplayer.get_unique_id():
