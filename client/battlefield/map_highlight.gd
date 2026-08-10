2feb23397ff11d2acb4ec1b1d5a3e68539fe2c46
Mursupaani <antti.pollanen89@gmail.com>
2026-08-10 17:28:49 +0300

    refactor(client): Player controller handles player actions. Map highligh handles highlight

--- client/battlefield/map_highlight.gd
+++ client/battlefield/map_highlight.gd
@@ -1,8 +1,18 @@
 extends HexagonTileMapLayer
 
-# Fix this!
-# func _on_hex_broadcast(peer_id: int, hex: Vector2i) -> void:
-# 	print("here here here")
-# 	clear()
-# 	set_cell(hex, 0, Vector2i(0, 0))
-#
+var player_hex := {}
+
+func _ready() -> void:
+	Network.Match.hex_broadcast.connect(_on_hex_broadcast)
+
+func _on_hex_broadcast(peer_id: int, hex: Vector2i) -> void:
+	if peer_id == multiplayer.get_unique_id():
+		clear()
+		set_cell(hex, 0, Vector2i(0, 0))
+	#else:
+	#	return
+	clear()
+	# Highlights both players' selections
+	player_hex[peer_id] = hex
+	for id in player_hex.keys():
+		set_cell(player_hex[id], 0, Vector2i(0, 0))
