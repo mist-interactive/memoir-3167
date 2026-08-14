88279fcc650661419ef4f5bcbf72999c057a8c34
jackie jan <zakaria.farah55@outlook.com>
2026-08-15 00:25:12 +0300

    chore: nuked the game menu

--- client/play.gd
+++ client/play.gd
@@ -1,10 +1,11 @@
 extends Node2D
-@onready var webClient: WebClient = get_node("../../../../../Client")
+@export var webClient: WebClient
 @export var findGameBtn: Button
 var queuing: bool = false
 
 func _ready() -> void:
 	webClient.connection_status_change.connect(_on_connection_status_change)
+	hide()
 
 func _on_find_game_button_down() -> void:
 	if !webClient.connected:
@@ -17,11 +18,6 @@ func _on_find_game_button_down() -> void:
 		queuing = false
 		findGameBtn.text = "Find game"
 	pass
-
-
-func _on_button_button_down() -> void:
-	print("jere")
-	pass # Replace with function body.
 
 func _on_connection_status_change() -> void:
 	if queuing && !webClient.connected:
