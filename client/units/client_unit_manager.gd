12af2d352c527bd8aca5b4798e00083d21bd77af
jackie jan <zakaria.farah55@outlook.com>
2026-08-17 01:04:20 +0300

    feat: clients able to join their match back after disconnecting

--- client/units/client_unit_manager.gd
+++ client/units/client_unit_manager.gd
@@ -5,11 +5,18 @@ var active_container: Node
 
 var UNIT_SCENE = preload("res://client/units/Unit.tscn")
 
-func _init(initialState: BattlefieldState) -> void:
+func _init(initialState: BattlefieldState, snapshot: Dictionary = {}) -> void:
 	super(initialState)
 	Network.Units.sync_unit_requested.connect(_on_sync_unit_requested)
 	Network.Units.sync_all_requested.connect(_on_sync_all_requested)
 	Network.Units.spawn_unit_requested.connect(_on_spawn_unit_requested)
+
+func initialize(active_container: Node, snapshot: Dictionary = {}) -> void:
+	self.active_container = active_container
+	if !snapshot.is_empty() && snapshot.has("units"):
+		print("snapshot.units", snapshot.units)
+		for unit: Dictionary in snapshot.units:
+			_on_spawn_unit_requested(unit)
 
 func _on_sync_unit_requested(snapshot: Dictionary) -> void:
 	var uuid: int = snapshot.uuid
@@ -20,6 +27,9 @@ func _on_sync_unit_requested(snapshot: Dictionary) -> void:
 func _on_sync_all_requested(snapshot: Dictionary):
 	selected_unit_id = snapshot.selected_unit_id
 	selected_by_peer = snapshot.selected_by_peer
+	unit_grid.clear()
+	for unit: Unit in units_by_id.values():
+		unit_grid[unit.hex_coord] = unit.uuid
 
 func _on_spawn_unit_requested(unit: Dictionary) -> void:
 	var new_unit = UNIT_SCENE.instantiate() as Unit
