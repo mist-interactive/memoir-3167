33ffe17e020b8b9bddabb3bf3d4e77bec1a3d8a9
Usva Pellinen <julius.pellinen@gmail.com>
2026-08-18 16:27:39 +0300

    fix: centerd healtbar

--- client/units/health_bar.gd
+++ client/units/health_bar.gd
@@ -3,6 +3,8 @@ extends TextureProgressBar
 @export var unit: Unit
 
 func _ready() -> void:
+	set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	pivot_offset = size / 2.0
 	var tile_scale = HexMetrics.tile_width / HexMetrics.HEX_SIZE
-	scale = Vector2(tile_scale / 2, tile_scale)
-	position = unit.hex_coord + Vector2i(-HexMetrics.tile_width, -HexMetrics.tile_height / 2.0)
+	scale = Vector2(tile_scale / 2.0, tile_scale)
+	position.y = -HexMetrics.half_height
