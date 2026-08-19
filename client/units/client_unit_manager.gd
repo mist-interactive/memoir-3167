170a94d503ed0386ca17ce526271d82cb1567907
jackie jan <zakaria.farah55@outlook.com>
2026-08-19 20:02:08 +0300

    feat: added helper to get hex world position

--- client/units/client_unit_manager.gd
+++ client/units/client_unit_manager.gd
@@ -4,6 +4,7 @@ class_name ClientUnitManager
 var active_container: Node
 @onready var match_state: MatchState = $"../matchState"
 var UNIT_SCENE = preload("res://client/units/Unit.tscn")
+var map_manager: ClientMapManager
 
 func _init(initialState: BattlefieldState) -> void:
 	super(initialState)
@@ -14,8 +15,9 @@ func _init(initialState: BattlefieldState) -> void:
 	unit_selected.connect(_on_unit_selected)
 	
 # snapshot used for reconnection
-func initialize(active_container: Node, snapshot: Dictionary = {}) -> void:
+func initialize(active_container: Node, map_manager: ClientMapManager, snapshot: Dictionary = {}) -> void:
 	self.active_container = active_container
+	self.map_manager = map_manager
 	if !snapshot.is_empty() && snapshot.has("units"):
 		print("snapshot.units", snapshot.units)
 		for unit: Dictionary in snapshot.units:
@@ -45,8 +47,10 @@ func _on_spawn_unit_requested(unit: Dictionary) -> void:
 func _on_resolve_combat_result_requested(result: CombatResult) -> void:
 	var uuid: int = result.unit_ids[enums.Side.RED] if enums.Side.GREEN == result.target else result.unit_ids[enums.Side.GREEN]
 	var assault_unit: Unit = units_by_id[uuid]
+	var target_uuid: int = result.unit_ids[result.target]
+	var world_pos: Vector2 =  map_manager.get_unit_world_pos(units_by_id[target_uuid].hex_coord)
 	if assault_unit.type == enums.UnitType.TANK:
-		assault_unit.get_node("./Tank").attack(result.dmg)
+		assault_unit.get_node("./Tank").attack(world_pos, result.dmg)
 
 func _on_unit_selected(unit_id: int, prev_unit_id: int) -> void:
 	if match_state.phase == enums.TurnPhase.ATTACK && unit_id != -1 && units_by_id[unit_id].type == enums.UnitType.TANK:
