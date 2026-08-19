f1e835b391da030163a7b098465017871fbc3bf0
jackie jan <zakaria.farah55@outlook.com>
2026-08-19 20:04:56 +0300

    feat: added explosive ammunition to tanks

--- client/units/Tanks/assault_tanks.gd
+++ client/units/Tanks/assault_tanks.gd
@@ -1,15 +1,18 @@
 extends Node2D
 @onready var tanks: Array[Node2D] = [$left, $mid, $right]
-
+var cooldown: int = 1.5
 func scan(num: int = 3) -> void:
 	for i in range(num):
 		tanks[i].scan()
 
-func attack(num: int) -> void:
+func attack(pos: Vector2, num: int) -> void:
 	var i: int = 0
 	while i < 3:
 		if i < num:
-			tanks[i].shoot()
+			var timer = get_tree().create_timer(cooldown)
+			tanks[i].shoot(pos)
+			await timer.timeout
+			tanks[i].idle()
 		else:
 			tanks[i].idle()
 		i += 1
