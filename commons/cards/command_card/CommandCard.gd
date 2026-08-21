953ba7abdde701dfba46087b1aa1f25bb834a2dc
Usva <julius.pellinen@gmail.com>
2026-08-21 15:54:31 +0300

    added second desc label to commandcard

--- commons/cards/command_card/CommandCard.gd
+++ commons/cards/command_card/CommandCard.gd
@@ -5,12 +5,10 @@ extends Resource
 @export var id: String = ""
 @export var title_label: String = ""
 @export_multiline var description_label: String = ""
-@export var target_sector: enums.CardTargetSector = enums.CardTargetSector.ALL
+@export_multiline var description_label_bottom: String = ""
+@export var target_sector: enums.CardTargetSector = enums.CardTargetSector.NONE
 @export var deck_quantity: int = 1
-@export var target_unit: enums.UnitType = enums.UnitType.INFANTRY
+@export var target_unit: enums.UnitType = enums.UnitType.ANY
 @export var target_unit_limit: int = 1
 @export_group("Visuals")
 @export var card_art: Texture2D
-
-# creating a .tres card file
-# (Right-Click FileSystem -> Create New -> Resource -> search for "CommandCard"), 
