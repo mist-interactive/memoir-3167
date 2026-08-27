6756bfe1bd5099eab43651f53b7674df0b7a9d4c
Mursupaani <antti.pollanen89@gmail.com>
2026-08-27 13:27:57 +0300

    refactor(units): enums.UnitType is now a bitflag to help in the future e.g. TerrainDatabase

--- commons/autoloads/unit_database/unit_database.gd
+++ commons/autoloads/unit_database/unit_database.gd
@@ -1,13 +1,13 @@
 extends Node
 
-@export var registered_units: Array[UnitStats] = []
+@export var registered_units: Dictionary[enums.UnitType, UnitStats] = {}
 
 var _db: Dictionary[enums.UnitType, UnitStats] = {}
 
 func _ready() -> void:
 	for stats in registered_units:
 		if stats != null:
-			_db[stats.type] = stats
+			_db[stats] = registered_units[stats]
 
 func get_stats(unit_type: enums.UnitType) -> UnitStats:
 	if _db.has(unit_type):
