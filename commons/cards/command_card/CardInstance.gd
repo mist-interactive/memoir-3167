fac3c7b922f5d9567a14ed5b4d1195ee668757f6
jackie jan <zakaria.farah55@outlook.com>
2026-09-01 16:55:57 +0300

    refactor: card instance stores owner of the card

--- commons/cards/command_card/CardInstance.gd
+++ commons/cards/command_card/CardInstance.gd
@@ -3,16 +3,19 @@ extends RefCounted
 
 var instance_id: int
 var card_id: String
+var owner_side: enums.Side
 
-func _init(instance_id: int, card_id: String):
+func _init(instance_id: int, card_id: String, owner_side: enums.Side):
 	self.instance_id = instance_id
 	self.card_id = card_id
+	self.owner_side = owner_side
 
 func to_dict() -> Dictionary:
 	return {
 		"instance_id": instance_id,
-		"card_id": card_id
+		"card_id": card_id,
+		"owner_side": owner_side
 	}
 
 static func from_dict(d: Dictionary) -> CardInstance:
-	return CardInstance.new(d["instance_id"], d["card_id"])
+	return CardInstance.new(d["instance_id"], d["card_id"], d["owner_side"])
