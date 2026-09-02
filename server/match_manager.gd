3a7bda4e9143136e8eb981b38608bbcb91d33347
jackie jan <zakaria.farah55@outlook.com>
2026-09-02 20:08:25 +0300

    wip(refactor): moving queue system/matching out of game server and reconnection

--- server/match_manager.gd
+++ server/match_manager.gd
@@ -8,9 +8,12 @@ var matches: Dictionary[int, matchController] = {}
 @onready var server: Server = $".."
 
 func _ready() -> void:
-	Network.Match.connect_match_requested.connect(_on_player_connect)
 	Network.Match.update_client_match_change_requested.connect(_on_client_match_state_change)
 	Network.Match.sync_requested.connect(_on_sync_match_state)
+	# Connection
+	Network.Match.connect_match_requested.connect(_on_player_connect)
+	Network.Match.client_ready_requested.connect(_on_client_ready)
+	Network.Match.client_game_ready_requested.connect(_on_client_game_ready)
 	# Action signals
 	Network.Actions.draw_hand_requested.connect(_on_draw_hand)
 	Network.Actions.play_card_requested.connect(_on_play_card)
@@ -20,19 +23,25 @@ func _ready() -> void:
 	Network.Actions.draw_card_requested.connect(_on_draw_card)
 	Network.Actions.continue_to_next_phase_requested.connect(_on_continue_to_next_phase_requested)
 
-func create_new_match(peerId1: int, peerId2: int, uuid1: int, uuid2: int) -> void:
-	var matchNode: matchController = matchController.new(_next_match_id, peerId1, peerId2)
-	matchNode.logger = server.logger.with_context({"match": _next_match_id})
+func create_new_match(match_id: int) -> void:
+	var matchNode: matchController = matchController.new(match_id)
+	matchNode.logger = server.logger.with_context({"match": match_id})
 	get_parent().add_child(matchNode)
-	peer_to_match[peerId1] = _next_match_id
-	peer_to_match[peerId2] = _next_match_id
-	uuid_to_peer[uuid1] = peerId1
-	uuid_to_peer[uuid2] = peerId2
-	matches[_next_match_id] = matchNode
-	_next_match_id += 1
-	Network.Match.match_created.rpc_id(peerId1)
-	Network.Match.match_created.rpc_id(peerId2)
+	matches[match_id] = matchNode
 	matchNode.logger.info("Created match")
+
+func join_game(peer_id: int, uuid: int, match_id: int) -> void:
+	peer_to_match[peer_id] = match_id
+	uuid_to_peer[uuid] = peer_id
+
+func get_peer_id(uuid: int) -> int:
+	return uuid_to_peer[uuid]
+
+func get_uuid(peer_id: int) -> int:
+	for uuid in uuid_to_peer:
+		if uuid_to_peer[uuid] == peer_id:
+			return uuid
+	return -1
 
 func reconnect(peer_id: int, uuid: int) -> void:
 	var old_peer_id: int = uuid_to_peer[uuid]
@@ -64,6 +73,8 @@ func client_disconnected(peer_id: int) -> void:
 	matchCtl.handle_disconnect(matchCtl.get_side(peer_id))
 
 func get_match(peer_id: int) -> matchController:
+	if !peer_to_match.has(peer_id):
+		return null
 	var matchId: int = peer_to_match[peer_id]
 	var matchCtl: matchController = matches[matchId]
 	return matchCtl
@@ -76,28 +87,39 @@ func _on_sync_match_state(peer_id: int, snapshot: Dictionary):
 	var matchCtl: matchController = get_match(peer_id)
 	matchCtl.matchState.sync_with_server(snapshot)
 
-func _on_player_connect(peer_id: int) -> void:
+func _on_player_connect(peer_id: int, uuid: int, match_id: int) -> void:
+	server.logger.info("Client(%d) wants to connect to match(%d)" % [uuid, match_id])
+	if !matches.has(match_id):
+		create_new_match(match_id)
+	join_game(peer_id, uuid, match_id)	
 	var matchCtl: matchController = get_match(peer_id)
-	matchCtl.handle_connect(peer_id)
-	var snapshot: Dictionary = {
-		"state": matchCtl.matchState.state,
-		"match_id": matchCtl.matchState.matchId,
-		"map_name": matchCtl.battlefield.mapName
-	}
-	Network.Match.init.rpc_id(peer_id, snapshot)
+	matchCtl.handle_connect(uuid, peer_id)
 
 func _on_player_disconnect(peer_id: int) -> void:
 	var matchCtl: matchController = get_match(peer_id)
 	matchCtl.handle_disconnect(peer_id)
 
-func _on_connect_match_requested(peer_id: int) -> void:
+func _on_client_ready(peer_id: int) -> void:
 	var matchCtl: matchController = get_match(peer_id)
-	var snapshot: Dictionary = {
-		"state": matchCtl.matchState.state,
-		"match_id": matchCtl.matchState.matchId,
-		"map_name": matchCtl.battlefield.mapName
-	}
-	Network.Match.init.rpc_id(peer_id, snapshot)
+	var uuid: int = get_uuid(peer_id)
+	assert(uuid != -1)
+	matchCtl.handle_client_ready(uuid)
+
+func _on_client_game_ready(peer_id: int) -> void:
+	var matchCtl: matchController = get_match(peer_id)
+	var uuid: int = get_uuid(peer_id)
+	assert(uuid != -1)
+	matchCtl.handle_client_game_ready(uuid)
+
+func _on_connect_match_requested(peer_id: int, uuid: int, match_id: int) -> void:
+	server.logger.info("Client(%d) wants to connect to match(%d)" % [uuid, match_id])
+	#var matchCtl: matchController = get_match(peer_id)
+	#var snapshot: Dictionary = {
+		#"state": matchCtl.matchState.state,
+		#"match_id": matchCtl.matchState.matchId,
+		#"map_name": matchCtl.battlefield.mapName
+	#}
+	#Network.Match.init.rpc_id(peer_id, snapshot)
 
 func _on_client_match_state_change(peer_id: int, state: MatchState.STATE):
 	var matchCtl: matchController = get_match(peer_id)
