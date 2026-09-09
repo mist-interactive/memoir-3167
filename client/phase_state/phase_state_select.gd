2fe206170d2b1846bb16fbf372cf2e53e3319872
Zakaria Farah <zakaria.farah55@outlook.com>
2026-09-09 16:54:45 +0300

    feat: client able to deselect unit

--- client/phase_state/phase_state_select.gd
+++ client/phase_state/phase_state_select.gd
@@ -4,7 +4,7 @@ extends PhaseState
 func handle_left_click(hex: Vector2i) -> void:
 	super.handle_left_click(hex)
 	if controller.matchState.is_my_turn():
-		var selected_unit: Unit = controller.selected_unit
+		var selected_unit: Unit = controller.selected_unit # selected_unit should be tracked on server side as you lose this information on disconnect 
 		if not selected_unit:
 			return
 		var is_my_unit: bool = selected_unit.owner_id == controller.matchState.mySide
@@ -12,5 +12,4 @@ func handle_left_click(hex: Vector2i) -> void:
 			if not controller.unit_manager.selected_units_ids.has(selected_unit.uuid):
 				Network.Actions.select_unit.rpc_id(1, controller.unit_manager.get_unit_at(hex).uuid)
 			else:
-				#NOTE: Add unit deselection here
-				print("Deselect unit (not functional yet)!")
+				Network.Actions.deselect_unit.rpc_id(1, controller.unit_manager.get_unit_at(hex).uuid) # select again and server deselects
