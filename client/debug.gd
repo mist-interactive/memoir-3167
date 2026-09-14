c8fbb446ea92a11aee2adc5ec0c242eecb20a56a
Usva Pellinen <julius.pellinen@gmail.com>
2026-09-14 18:34:00 +0300

    updates

--- client/debug.gd
+++ client/debug.gd
@@ -9,7 +9,11 @@ extends Control
 @onready var scores = $ResizeUI/Scores
 @onready var winner = $ResizeUI/Winner
 @onready var button = $ResizeUI/Button
+@onready var next_phase = $ResizeUI/NextPhase
 @onready var ui = $ResizeUI
+
+@export var bottom_colored_bar: TextureRect
+@export var top_colored_bar: TextureRect
 
 const DESIGN_SIZE := Vector2(1920, 1080)
 var debug_hidden: bool = false
@@ -36,6 +40,16 @@ func _ready() -> void:
 	update_ui()
 	update_score_pips()
 
+func update_player_color() -> void:
+	match matchState.mySide:
+		enums.Side.RED:
+			bottom_colored_bar.modulate = Color(0.429, 0.07, 0.08, 1.0)
+			top_colored_bar.modulate = Color(0.029, 0.37, 0.08, 1.0)
+		enums.Side.GREEN:
+			bottom_colored_bar.modulate = Color(0.029, 0.37, 0.08, 1.0)
+			top_colored_bar.modulate = Color(0.429, 0.07, 0.08, 1.0)
+		enums.Side.NONE:
+			return
 
 func update_ui():
 	var viewport_size := get_viewport_rect().size
@@ -43,10 +57,8 @@ func update_ui():
 		viewport_size.x / DESIGN_SIZE.x,
 		viewport_size.y / DESIGN_SIZE.y
 	)
-
 	ui.scale = Vector2.ONE * scale_factor
 	ui.position = (viewport_size - DESIGN_SIZE * scale_factor) / 2.0
-
 
 func _physics_process(delta: float) -> void:
 	var server_now: float = clock.get_server_time()
@@ -55,6 +67,9 @@ func _physics_process(delta: float) -> void:
 		matchState.scores[enums.Side.RED],
 		matchState.scores[enums.Side.GREEN]
 	]
+	
+	if matchState.mySide != enums.Side.NONE:
+		update_player_color()
 
 	peer_ids.text = "Side: " + player_id_text(matchState.mySide)
 	phase.text = "turn phase: " + get_turn_phase_txt(matchState.phase) + "(%d)" % (count_down / 1000)
@@ -62,10 +77,18 @@ func _physics_process(delta: float) -> void:
 	turn.text = "player_turn: " + player_id_text(matchState.current_turn)
 	winner.text = "winner: " + player_id_text(matchState.winner)
 
-	if matchState.current_turn != matchState.mySide:
-		button.text = "Waiting"
+	var is_my_turn := matchState.current_turn == matchState.mySide
+
+	# Only show phase information during your turn.
+	phase.visible = is_my_turn
+	next_phase.visible = is_my_turn
+
+	if is_my_turn:
+		phase.text = get_turn_phase_txt(matchState.phase)
+		next_phase.text = "To " + get_turn_phase_txt(get_next_phase())
+		button.text = "Next"
 	else:
-		button.text = get_turn_phase_txt(matchState.phase)
+		button.text = "Opponent's Turn"
 
 	update_score_pips()
 
@@ -74,11 +97,36 @@ func _physics_process(delta: float) -> void:
 		debug_hidden = !debug_hidden
 
 
+func get_next_phase() -> enums.TurnPhase:
+	match matchState.phase:
+		enums.TurnPhase.SPAWN_UNITS:
+			return enums.TurnPhase.DRAW_HAND
+
+		enums.TurnPhase.DRAW_HAND:
+			return enums.TurnPhase.PLAY_CARD
+
+		enums.TurnPhase.PLAY_CARD:
+			return enums.TurnPhase.SELECT
+
+		enums.TurnPhase.SELECT:
+			return enums.TurnPhase.MOVE
+
+		enums.TurnPhase.MOVE:
+			return enums.TurnPhase.ATTACK
+
+		enums.TurnPhase.ATTACK:
+			return enums.TurnPhase.DRAW_CARD
+
+		enums.TurnPhase.DRAW_CARD:
+			return enums.TurnPhase.SPAWN_UNITS
+
+	return enums.TurnPhase.SPAWN_UNITS
+
+
 func update_score_pips() -> void:
 	var my_score: int = matchState.scores[matchState.mySide]
 	var enemy_side: enums.Side
 
-	# Determine the enemy side
 	if matchState.mySide == enums.Side.RED:
 		enemy_side = enums.Side.GREEN
 	elif matchState.mySide == enums.Side.GREEN:
@@ -102,20 +150,17 @@ func update_score_pips() -> void:
 		my_active_color = Color.WHITE
 		enemy_active_color = Color.WHITE
 
-	# Update your score pips
 	for i in range(score_pips.size()):
 		if i < my_score:
 			score_pips[i].modulate = my_active_color
 		else:
-			score_pips[i].modulate = Color(0.25, 0.25, 0.25, 1.0)
+			score_pips[i].modulate = Color(0.722, 0.722, 0.722, 1.0)
 
-	# Update enemy score pips
 	for i in range(score_pips_enemy.size()):
 		if i < enemy_score:
 			score_pips_enemy[i].modulate = enemy_active_color
 		else:
-			score_pips_enemy[i].modulate = Color(0.25, 0.25, 0.25, 1.0)
-
+			score_pips_enemy[i].modulate = Color(0.722, 0.722, 0.722, 1.0)
 
 
 func player_id_text(side: enums.Side) -> String:
@@ -134,17 +179,18 @@ func get_turn_phase_txt(phase: enums.TurnPhase) -> String:
 		enums.TurnPhase.DRAW_HAND:
 			return "Draw Hand"
 		enums.TurnPhase.PLAY_CARD:
-			return "Play Card"
+			return "Play a Card"
 		enums.TurnPhase.SELECT:
-			return "Select"
+			return "Select Units"
 		enums.TurnPhase.MOVE:
-			return "Move"
+			return "Move Units"
 		enums.TurnPhase.ATTACK:
 			return "Attack"
 		enums.TurnPhase.RESOLVE_RETREAT:
 			return "Retreat"
 		enums.TurnPhase.DRAW_CARD:
-			return "Draw Card"
+			return "Draw a Card"
+
 	return "Unknown"
 
 
@@ -162,6 +208,7 @@ func get_game_state_txt(state: MatchState.STATE) -> String:
 			return "Paused"
 		MatchState.STATE.ENDED:
 			return "Ended"
+
 	return "Unknown"
 
 
@@ -169,7 +216,9 @@ func _on_confirm() -> void:
 	match matchState.phase:
 		enums.TurnPhase.SELECT:
 			Network.Actions.continue_to_next_phase.rpc_id(1)
+
 		enums.TurnPhase.MOVE:
 			Network.Actions.continue_to_next_phase.rpc_id(1)
+
 		enums.TurnPhase.ATTACK:
 			Network.Actions.continue_to_next_phase.rpc_id(1)
