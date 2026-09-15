dbdace52eed0e576184a1724f15047672e151865
jackie jan <zakaria.farah55@outlook.com>
2026-09-15 23:12:26 +0300

    feat: highlighting of hexes happens in phase states process func

--- client/battlefield/unit_selection_highlight.gd
+++ client/battlefield/unit_selection_highlight.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	clear()
-	highlight_selected_units()
+	#highlight_selected_units()
 
 func highlight_selected_units() -> void:
 	for unit_id in unit_manager.selected_units_ids:
@@ -17,24 +17,24 @@ func highlight_selected_units() -> void:
 		if unit == null:
 			continue
 		highlight_cell(unit.hex_coord)
-#
-#func highlight_movable_units() -> void:
-	#for unit_id in unit_manager.selected_units_ids:
-		#var unit = unit_manager.get_unit_by_id(unit_id)
-		#if unit == null:
-			#continue
-		#if unit_id in unit_manager.moved_units_ids:
-			#continue
-		#highlight_cell(unit.hex_coord)
-#
-#func highlight_attackable_units() -> void:
-	#for unit_id in unit_manager.selected_units_ids:
-		#var unit = unit_manager.get_unit_by_id(unit_id)
-		#if unit == null:
-			#continue
-		#if unit_id in unit_manager.attacked_units_ids:
-			#continue
-		#highlight_cell(unit.hex_coord)
+
+func highlight_movable_units() -> void:
+	for unit_id in unit_manager.selected_units_ids:
+		var unit = unit_manager.get_unit_by_id(unit_id)
+		if unit == null:
+			continue
+		if unit_id in unit_manager.moved_units_ids:
+			continue
+		highlight_cell(unit.hex_coord)
+
+func highlight_attackable_units() -> void:
+	for unit_id in unit_manager.selected_units_ids:
+		var unit = unit_manager.get_unit_by_id(unit_id)
+		if unit == null:
+			continue
+		if unit_id in unit_manager.attacked_units_ids:
+			continue
+		highlight_cell(unit.hex_coord)
 
 func highlight_cell(coord: Vector2i) -> void:
 	set_cell(coord, 2, Vector2i(0, 0))
