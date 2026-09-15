c8f712c61806e443b24a064156c26fa48abdb726
jackie jan <zakaria.farah55@outlook.com>
2026-09-15 22:37:02 +0300

    feat: added retreat phase

---
+++ client/phase_state/phase_state_retreat.gd
@@ -0,0 +1,20 @@
+class_name PhaseStateRetreat
+extends PhaseState
+
+func _process(delta: float) -> void:
+	controller.highlight_possible_retreats()
+
+func handle_left_click(hex: Vector2i) -> void:
+	pass
+
+func handle_right_click(hex: Vector2i) -> void:
+	for unit: Unit in controller.unit_manager.units_by_id.values():
+		if unit.num_of_retreat > 0 && unit.hex_coord != hex:
+			var tree: BinaryTree = controller.unit_manager.get_retreat_coords(unit.hex_coord, unit.num_of_retreat)
+			var coords: Array[Variant] = tree.to_array()
+			if coords.has(hex):
+				Network.Actions.retreat_unit.rpc_id(1, unit.uuid, hex)
+			break;
+
+func handle_mouse_motion(hex: Vector2i) -> void:
+	pass
