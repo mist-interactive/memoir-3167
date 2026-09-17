55c74ac6f00c459062bee3dcca2545df41175594
jackie jan <zakaria.farah55@outlook.com>
2026-09-17 12:07:46 +0300

    feat: added getter for getting node values by level

--- commons/data_structures/binary_tree/binary_tree.gd
+++ commons/data_structures/binary_tree/binary_tree.gd
@@ -8,6 +8,23 @@ var right: BinaryTree = null
 func _init(value: Variant) -> void:
 	self.value = value
 
+func get_nodes_by_level(level: int, filter_duplicate: bool = true) -> Array[Variant]:
+	var curr_level: int = 0
+	var arr: Array[Variant] = []
+	_get_nodes_by_level(self, arr, curr_level, level, filter_duplicate)
+	return arr
+
+func _get_nodes_by_level(node: BinaryTree, arr: Array[Variant], curr_level: int, target_level: int, filter_duplicate: bool):
+	if node == null:
+		return
+	if curr_level == target_level:
+		if filter_duplicate && arr.has(node.value):
+			return
+		arr.append(node.value)
+		return
+	_get_nodes_by_level(node.left, arr, curr_level + 1, target_level, filter_duplicate)
+	_get_nodes_by_level(node.right, arr, curr_level + 1, target_level, filter_duplicate)
+
 func find_node_level(target: Variant) -> int:
 	return _get_node_level(self, target, 0)
 
