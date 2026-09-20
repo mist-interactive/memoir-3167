0d8f3689d67823a550bdc2439f3f7deb12ba1579
Mursupaani <antti.pollanen89@gmail.com>
2026-09-20 12:30:07 +0300

    fix(unit visuals): Fixed unit formations

--- client/units/unit_visuals.gd
+++ client/units/unit_visuals.gd
@@ -19,21 +19,21 @@ const ANIMATION_MAP: Dictionary = {
 
 const FORMATIONS: Dictionary = {
 	enums.UnitType.INFANTRY: {
-		4: [Vector2(-24, -24), Vector2(24, -24), Vector2(24, 24), Vector2(-24, 24)],
-		3: [Vector2(0, -24), Vector2(-24, 12), Vector2(24, 12)],
+		4: [Vector2(-24, -32), Vector2(24, -32), Vector2(24, 16), Vector2(-24, 16)],
+		3: [Vector2(-24, -32), Vector2(24, -32), Vector2(0, 16)],
 		2: [Vector2(-24, 0), Vector2(24, 0)],
 		1: [Vector2(0, 0)],
 	},
 	enums.UnitType.TANK: {
 		4: [Vector2(-36, -36), Vector2(36, -36), Vector2(36, 36), Vector2(-36, 36)],
 		3: [Vector2(4, -50), Vector2(-28, -10), Vector2(25, 9)],
-		2: [Vector2(-36, 0), Vector2(36, 0)],
+		2: [Vector2(-32, -14), Vector2(32, -14)],
 		1: [Vector2(0, 0)],
 	},
 	enums.UnitType.ARTILLERY: {
 		4: [Vector2(-36, -36), Vector2(36, -36), Vector2(36, 36), Vector2(-36, 36)],
 		3: [Vector2(4, -50), Vector2(-28, -10), Vector2(25, 9)],
-		2: [Vector2(-36, 0), Vector2(36, 0)],
+		2: [Vector2(-32, -14), Vector2(32, -14)],
 		1: [Vector2(0, 0)],
 	}
 }
@@ -67,8 +67,10 @@ static func apply_figure_visuals(unit_figure: Variant, owner_id: int, unit_type:
 		unit_figure.visible = false
 		return
 	var visual_data: Dictionary = ANIMATION_MAP[owner_id][unit_type]
-	var figure_offset = FORMATIONS.get(unit_type).get(unit_health)
-	unit_figure.position = figure_offset[index]
+	var figure_offset = FORMATIONS.get(unit_type).get(unit_health)[index]
+	if owner_id == 1:
+		figure_offset.x *= -1
+	unit_figure.position = figure_offset
 	unit_figure.z_index = index
 	unit_figure.anim_sprite.offset = visual_data.get("offset")
 	unit_figure.anim_sprite.play(visual_data.get("anim"))
