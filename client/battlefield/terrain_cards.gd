dd355a05bfb7f452d57a86e635609aea8676e6ea
Usva Pellinen <julius.pellinen@gmail.com>
2026-09-22 19:50:59 +0300

    fix(ui) twewaks to terraincards

--- client/battlefield/terrain_cards.gd
+++ client/battlefield/terrain_cards.gd
@@ -7,9 +7,6 @@ class_name TerrainCards
 @export var map_ground_layer: TileMapLayer
 var _current_terrain_card: TerrainCardUI = null
 
-func _ready() -> void:
-	size = Vector2(HandUI.card_size.y, HandUI.card_size.x)
-
 func restore_size() ->void:
 	ui_control.restore()
 
@@ -42,8 +39,6 @@ func display_terrain_card(hex: Vector2) -> void:
 		new_card.setup_visuals(str(terrain_id))
 
 	new_card.global_position = Vector2.ZERO
-	new_card.size = Vector2(HandUI.card_size.y, HandUI.card_size.x)
-	new_card.scale = Vector2.ONE
 
 func clear_terrain_card() -> void:
 	if is_instance_valid(_current_terrain_card):
