911892b4e157a1a4d6fb610865f0aeccf162c21d
Mursupaani <antti.pollanen89@gmail.com>
2026-09-25 17:07:00 +0300

    fix(info_screen): Fixed missing info screen / pane

--- client/battlefield/info_screen.gd
+++ client/battlefield/info_screen.gd
@@ -1,18 +1,25 @@
 extends CanvasLayer
 @onready var match_state: MatchState = $"../../matchState"
+@export var info_pane: Node2D
 @export var info_text: RichTextLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
 	match_state.match_state_changed.connect(on_match_state_changed)
+	get_viewport().size_changed.connect(_center_info_pane)
 	info_text.clear()
-	pass # Replace with function body.
+	info_text.set_anchors_preset(Control.PRESET_CENTER)
+	info_text.fit_content = true
+	info_text.autowrap_mode = TextServer.AUTOWRAP_OFF
+	_center_info_pane()
 
 func on_match_state_changed(new_state: MatchState.STATE):
 	if new_state == MatchState.STATE.ENDED:
 		var winner: int = match_state.get_winner()
-		info_text.text = "Player " + str(winner) + " wins!"
+		if winner == match_state.mySide:
+			info_text.text = "You win!"
+		else:
+			info_text.text = "You lose!"
 		visible = true
 	elif new_state == MatchState.STATE.PAUSED:
 		info_text.text = "Game paused"
@@ -20,4 +27,7 @@ func on_match_state_changed(new_state: MatchState.STATE):
 	elif new_state == MatchState.STATE.IN_PROGRESS:
 		info_text.clear()
 		visible = false
-	pass
+
+func _center_info_pane() -> void:
+	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
+	info_pane.position = (viewport_size / 2)
