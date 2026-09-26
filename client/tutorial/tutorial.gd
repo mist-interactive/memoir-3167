a97b05fdddee944bef553fedc82afb8b5f31f40c
Usva Pellinen <julius.pellinen@gmail.com>
2026-09-26 17:25:54 +0300

    feat(tutorial) added drop down menu with page buttons

--- client/tutorial/tutorial.gd
+++ client/tutorial/tutorial.gd
@@ -2,8 +2,9 @@ extends Control
 
 @export var minimize_button: Button
 @export var tutorial_button: Button
+@export var menu_button: Button
 @export var panel: Panel
-@export var text: Label
+@export var menu_panel: Panel
 
 @export var page_buttons_container: Container
 
@@ -14,25 +15,20 @@ const DESIGN_SIZE := Vector2(1920, 1080)
 
 var current_page := 0
 
-
 func _ready() -> void:
 	tutorial_button.visible = false
-
 	minimize_button.pressed.connect(_on_minimize_pressed)
 	tutorial_button.pressed.connect(_on_tutorial_pressed)
-
+	menu_button.pressed.connect(_on_menu_pressed)
 	for i in range(page_buttons.size()):
 		page_buttons[i].pressed.connect(_on_page_button_pressed.bind(i))
-
 	get_viewport().size_changed.connect(update_ui)
 	update_ui()
-
 	show_page()
 
 
 func update_ui() -> void:
 	var viewport_size := get_viewport_rect().size
-
 	var scale_factor := minf(
 		viewport_size.x / DESIGN_SIZE.x,
 		viewport_size.y / DESIGN_SIZE.y
@@ -41,39 +37,33 @@ func update_ui() -> void:
 func show_page() -> void:
 	for page in tutorial_pages:
 		page.visible = false
-
 	if tutorial_pages.size() > 0:
 		tutorial_pages[current_page].visible = true
-
 
 func _on_page_button_pressed(page_index: int) -> void:
 	if page_index >= 0 and page_index < tutorial_pages.size():
 		current_page = page_index
 		show_page()
 
-
 func _on_minimize_pressed() -> void:
 	panel.visible = false
-
 	for page in tutorial_pages:
 		page.visible = false
-
 	for button in page_buttons:
 		button.visible = false
-
 	minimize_button.visible = false
 	tutorial_button.visible = true
 	page_buttons_container.visible = false
-
 
 func _on_tutorial_pressed() -> void:
 	panel.visible = true
 	minimize_button.visible = true
 	tutorial_button.visible = false
 	page_buttons_container.visible = true
-	
-
 	for button in page_buttons:
 		button.visible = true
-
 	show_page()
+
+func _on_menu_pressed() -> void:
+	page_buttons_container.visible = !page_buttons_container.visible
+	menu_panel.visible = !menu_panel.visible
