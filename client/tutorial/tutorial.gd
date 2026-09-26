85a06622587c2b7d6277e6d3697bcdbc4822e2c8
Usva Pellinen <julius.pellinen@gmail.com>
2026-09-26 19:57:10 +0300

    fix(tutorial) tweaks and cheats

--- client/tutorial/tutorial.gd
+++ client/tutorial/tutorial.gd
@@ -14,12 +14,16 @@ extends Control
 @export var page_buttons: Array[Button]
 
 const DESIGN_SIZE := Vector2(1920, 1080)
-const DEFAULT_POSITION := Vector2.ZERO
 
 enum UIState {
 	TUTORIAL,
 	MINIMIZED,
 }
+
+const PAGE_SIZE := Vector2(448.0, 224.0)
+const PAGE_POSITION := Vector2(16.0, 32.0)
+const PAGE_CUSTOM_MIN := Vector2(448.0, 224.0)
+const PAGE_CUSTOM_MAX := Vector2(448.0, 224.0)
 
 var current_state := UIState.TUTORIAL
 var current_page := 0
@@ -37,6 +41,15 @@ func _ready() -> void:
 	for i in range(page_buttons.size()):
 		page_buttons[i].pressed.connect(_on_page_button_pressed.bind(i))
 
+	for page in tutorial_pages:
+		page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+		var rich_text_label: RichTextLabel = page.get_node("Text")
+		rich_text_label.size = PAGE_SIZE
+		rich_text_label.position = PAGE_POSITION
+		rich_text_label.custom_minimum_size = PAGE_CUSTOM_MIN
+		rich_text_label.custom_maximum_size = PAGE_CUSTOM_MAX
+
 	top_bar.gui_input.connect(_on_top_bar_input)
 
 	set_ui_state(UIState.TUTORIAL)
@@ -48,6 +61,7 @@ func set_ui_state(new_state: UIState) -> void:
 	match current_state:
 		UIState.TUTORIAL:
 			_apply_tutorial_state()
+
 		UIState.MINIMIZED:
 			_apply_minimized_state()
 
@@ -62,13 +76,10 @@ func _apply_tutorial_state() -> void:
 	menu_button.visible = true
 	menu_button.disabled = false
 
-	page_buttons_container.visible = false
-	menu_panel.visible = menu_open
-
 	for button in page_buttons:
 		button.visible = true
 
-	show_page()
+	apply_menu_state()
 
 
 func _apply_minimized_state() -> void:
@@ -88,29 +99,40 @@ func _apply_minimized_state() -> void:
 
 	for page in tutorial_pages:
 		page.visible = false
+		page.process_mode = Node.PROCESS_MODE_DISABLED
 
 	for button in page_buttons:
 		button.visible = false
-
-	position = DEFAULT_POSITION
 
 
 func apply_menu_state() -> void:
 	if current_state != UIState.TUTORIAL:
 		menu_panel.visible = false
 		page_buttons_container.visible = false
+
+		for page in tutorial_pages:
+			page.visible = false
+			page.process_mode = Node.PROCESS_MODE_DISABLED
+
 		return
 
 	menu_panel.visible = menu_open
 	page_buttons_container.visible = menu_open
 
+	for i in range(tutorial_pages.size()):
+		var page := tutorial_pages[i]
+		var active := i == current_page and not menu_open
+
+		page.visible = active
+
+		if active:
+			page.process_mode = Node.PROCESS_MODE_INHERIT
+		else:
+			page.process_mode = Node.PROCESS_MODE_DISABLED
+
 
 func show_page() -> void:
-	for page in tutorial_pages:
-		page.visible = false
-
-	if tutorial_pages.size() > 0:
-		tutorial_pages[current_page].visible = true
+	apply_menu_state()
 
 
 func _on_page_button_pressed(page_index: int) -> void:
@@ -118,7 +140,6 @@ func _on_page_button_pressed(page_index: int) -> void:
 		current_page = page_index
 		menu_open = false
 
-		show_page()
 		apply_menu_state()
 
 
@@ -128,6 +149,14 @@ func _on_minimize_pressed() -> void:
 
 func _on_tutorial_pressed() -> void:
 	set_ui_state(UIState.TUTORIAL)
+
+	menu_open = false
+
+	for page in tutorial_pages:
+		page.visible = false
+		page.process_mode = Node.PROCESS_MODE_DISABLED
+
+	apply_menu_state()
 
 
 func _on_menu_pressed() -> void:
